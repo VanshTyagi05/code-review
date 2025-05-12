12730f8deff6d45fcc36fef631e20dead6fdf1a0
Vansh Tyagi <vansht.ug23.ec@nitp.ac.in>
2025-05-12 16:38:24 +0530

    fix cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,24 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-const express = require('express');
-const aiRoutes=require('./routes/ai.routes');
-const cors=require('cors');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const express = require("express");
+const aiRoutes = require("./routes/ai.routes");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(cors());
-app.use('/ai',aiRoutes);
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.use(
+  cors({
+    origin: [
+      "https://code-review-0m9o.onrender.com",
+      "http://localhost:5173", // for local development
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use("/ai", aiRoutes);
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
-module.exports =app;
+module.exports = app;
